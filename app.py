bdab66deae9d3e19eb32a8f971083a41a28a2b16
Baoyi <baoyi@pku.edu.cn>
2022-02-27 18:17:41 +0800

    remove menu

--- app.py
+++ app.py
@@ -9,13 +9,23 @@ app = MultiApp()
 # """)
 
 # Add all your application here
-
 st.set_page_config(
         page_title="Patterns of spatially refined urban built environment stocks across Chinese cities",
         page_icon="🌎",
         layout="wide",
         initial_sidebar_state="expanded",
     )
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
 app.add_app("Home", home.app)
 app.add_app("Spatial Distribution", SpatialView.app)
 app.add_app("Building Stock Statistics Data Visualization", DataView.app)
